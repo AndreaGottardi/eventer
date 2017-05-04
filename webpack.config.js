49ccb596d6b434d46c1b463a455af7b299a78e7e
Daniele Borra <daniele.borra.93@gmail.com>
2017-05-04 17:57:26 +0200

    Added production minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var path = require('path');
+var env = process.env.WEBPACK_ENV;
 
 module.exports = {
 	entry: './src/eventer.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'eventer.js',
+		filename: env=='build' ? 'eventer.js' : 'eventer.min.js',
 		library: 'Eventer',
 		libraryTarget: 'umd',
 		umdNamedDefine: true
